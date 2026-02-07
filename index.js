45d01fd654369012984ea2d734646152d8b985b1
Arnab Mukherjee <mukherjeearnab988@gmail.com>
2026-02-07 19:30:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ const PORT = process.env.PORT || 5000;
 
 // ===== CORS Configuration =====
 const corsOptions = {
-  origin: ["*","http://localhost:8080", `${process.env.FRONTEND_URL}` ],
+  origin: ["https://v1-heritage-repository.vercel.app","http://localhost:8080", `${process.env.FRONTEND_URL}` ],
   methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
